dd703e0232a63dda354b63286f197e8594892144
joharbatta <joharbatta@gmail.com>
2020-03-15 14:00:23 +0530

    css

--- src/Components/redux/App.js
+++ src/Components/redux/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import Counter from './Counter';
+import './App.css';
 function App () {
   return (
     <Provider store={store}>
